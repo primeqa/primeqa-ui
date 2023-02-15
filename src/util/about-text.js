0ad0f62a33e3b707c56965b3cdddc5c898507318
evehling <evehling@ibm.com>
2023-02-15 15:10:41 -0800

    edit text

--- src/util/about-text.js
+++ src/util/about-text.js
@@ -1,9 +1,11 @@
+import { AppDeveloper, MachineLearning_01, Microscope } from '@carbon/pictograms-react';
+
 import { Information } from "./model";
 
 const about_information = [
-    new Information(0,"Train & Demo Models", "Researchers can use PrimeQA to both train custom QA models and demo them to a general audience.", "", ""),
-    new Information(1, "Replicate Experiments", "Academics & researchers can replicate experiments or download pre-trained models to test out on custom data."),
-    new Information(2, "Develop & Embed", "Embed the capabilities of PrimeQA state-of-the art models into a custom application without requiring training.")
+    new Information(0,"Train & Demo Models", "Researchers can use PrimeQA to both train custom QA models and demo them to a general audience.", <MachineLearning_01/>, ""),
+    new Information(1, "Replicate Experiments", "Academics & researchers can replicate experiments or download pre-trained models to test out on custom data.", <Microscope />),
+    new Information(2, "Develop & Embed", "Embed the capabilities of PrimeQA state-of-the art models into a custom application without requiring training.", <AppDeveloper />)
 ]
 
 export default about_information;
--- src/util/models.js
+++ src/util/models.js
@@ -3,7 +3,7 @@ import { IbmWatsonNaturalLanguageUnderstanding, Query, SearchLocate } from "@car
 import {Model} from "./model";
 
 const models = [
-    new Model("reading", "Machine Reading Comprehension", "Extract and/or generate answers given the source document or passage.", <SearchLocate size={32}/>, "#", []),
+    new Model("reading", "Reading Comprehension", "Extract and/or generate answers given the source document or passage.", <SearchLocate size={32}/>, "#", []),
     new Model("retrieval", "Information Retrieval", "Retrieve documents and passages using both traditional and neural models", <IbmWatsonNaturalLanguageUnderstanding size={32}/>, "#", []),
     new Model("question_gen", "Question Generation", "Supports generation of questions for effective domain adaptation over tables and multilingual text.", <Query size={32}/>, "#", [])
 ]
