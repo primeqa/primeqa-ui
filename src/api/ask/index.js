86322d86e88e32d09235a493a499d74ad3b91087
evehling <evehling@ibm.com>
2023-02-23 10:37:25 -0800

    added demo ask api call

--- src/api/ask/index.js
+++ src/api/ask/index.js
@@ -16,10 +16,9 @@
  *
  */
 
-import axios from "axios";
-import _ from "lodash";
-
 import { BASE_URL } from "../config";
+import _ from "lodash";
+import axios from "axios";
 
 /**
  * Trigger [POST] /ask API
@@ -43,6 +42,78 @@ export async function askQuestion(question, retriever, collection, reader) {
     retriever: retriever,
     collection: collection,
     reader: reader,
+  };
+
+  return axios
+    .post(url, data, config)
+    .then((response) => response.data)
+    .catch((error) => Promise.reject(_.get(error, "response.data") || error));
+}
+
+/**
+ * @param {String} question
+ * @param {[String]} contexts
+ */
+export async function demo_ask(question, contexts) {
+  const config = {
+    headers: { Accept: "application/json", "Content-Type": "application/json" },
+  };
+
+  let url = "http://9.59.151.58:50059/applications/primeqa/GetAnswersRequest";
+
+  let data = {
+    question: question,
+    contexts: contexts,
+    reader: {
+      "reader_id": "ExtractiveReader",
+      "parameters": [
+        {
+          "parameter_id": "model",
+          "name": "Model",
+          "description": "",
+          "type": "String",
+          "value": "PrimeQA/nq_tydi_sq1-reader-xlmr_large-20221110",
+          "options": [],
+          "range": []
+        },
+        {
+          "parameter_id": "max_num_answers",
+          "name": "Maximum number of answers",
+          "description": "",
+          "type": "Numeric",
+          "value": 3,
+          "options": [],
+          "range": [
+            1,
+            5,
+            1
+          ]
+        },
+        {
+          "parameter_id": "max_answer_length",
+          "name": "Maximum answer length",
+          "description": "",
+          "type": "Numeric",
+          "value": 1000,
+          "options": [],
+          "range": [
+            2,
+            2000,
+            2
+          ]
+        },
+        {
+          "parameter_id": "min_score_threshold",
+          "name": "Minimum score threshold",
+          "description": "",
+          "type": "Numeric",
+          "value": null,
+          "options": [],
+          "range": []
+        }
+      ],
+      "provenance":null
+    }
   };
 
   return axios
